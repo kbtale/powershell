ea1951352b4f416a532c68f1e2cdc349bab19547
Carlos Bolívar <cebemind@hotmail.com>
2026-04-26 16:40:33 -0400

    feat: setup custom theme logic to support playground component

---
+++ docs/.vitepress/theme/components/Playground.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps(['title', 'description', 'code', 'params'])
+</script>
+
+<template>
+  <div class="playground">
+    <p>Playground component for {{ title }}</p>
+  </div>
+</template>
+
+<style scoped>
+.playground {
+  margin-top: 2rem;
+  padding: 1rem;
+  border: 1px solid var(--vp-c-divider);
+  border-radius: 8px;
+}
+</style>
